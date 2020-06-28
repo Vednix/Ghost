ed52b4dd5f806fb56f786d7a314ab783d5eccc22
Vednix <contato@terrariabrasil.com.br>
2020-06-28 07:55:32 -0300

    Do Release

--- GhostPlugin/GhostPlugin.cs
+++ GhostPlugin/GhostPlugin.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.IO;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Reflection;
 using Terraria;
-using Terraria.Localization;
 using TerrariaApi.Server;
 using TShockAPI;
 
@@ -13,7 +12,7 @@ namespace Ghost
     {
         public override string Name => "Ghost";
 
-        public override string Author => "SirApples";
+        public override string Author => "SirApples (Mobile port by Vednix)";
 
         public override string Description => "A plugin that allows admins to become completely invisible to players.";
 
@@ -23,17 +22,21 @@ namespace Ghost
         {
             Commands.ChatCommands.Add(new Command("ghost.ghost", OnGhost, "ghost", "vanish"));
         }
+        public static int NewProjectile(Vector2 position, Vector2 velocity, int Type, int Damage, float KnockBack, int Owner = 16, float ai0 = 0.0f, float ai1 = 0.0f)
+        {
+            return Projectile.NewProjectile(position.X, position.Y, velocity.X, velocity.Y, Type, Damage, KnockBack, Owner, ai0, ai1);
+        }
         void OnGhost(CommandArgs args)
         {
-            int i = Projectile.NewProjectile(args.Player.LastNetPosition, Microsoft.Xna.Framework.Vector2.Zero, 170, 0, 0);
-            Main.projectile[i].timeLeft = 0;
-            NetMessage.SendData(27, -1, -1, null, i);
+            //int i = NewProjectile(args.Player.LastNetPosition, Vector2.Zero, 170, 0, 0);
+            //Main.projectile[i].timeLeft = 0;
+            //NetMessage.SendData(27, -1, -1, "", i);
             args.TPlayer.active = !args.TPlayer.active;
-            NetMessage.SendData(14, -1, args.Player.Index, null, args.Player.Index, args.TPlayer.active.GetHashCode());
+            NetMessage.SendData(14, -1, args.Player.Index, "", args.Player.Index, args.TPlayer.active.GetHashCode());
             if (args.TPlayer.active)
             {
-                NetMessage.SendData(4, -1, args.Player.Index, null, args.Player.Index);
-                NetMessage.SendData(13, -1, args.Player.Index, null, args.Player.Index);
+                NetMessage.SendData(4, -1, args.Player.Index, "", args.Player.Index);
+                NetMessage.SendData(13, -1, args.Player.Index, "", args.Player.Index);
             }
             args.Player.SendSuccessMessage($"{(args.TPlayer.active ? "Dis" : "En")}abled Vanish.");
         }
